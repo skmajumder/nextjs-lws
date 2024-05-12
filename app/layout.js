dd708935228e0c2103ba4e47752f6f3fe11f5583
skmajumder <skmajumder.cse@gmail.com>
2024-05-13 00:36:34 +0600

    Add Link component for navigation

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} p-6`}>
-        <nav>Home | About</nav>
+        <nav>
+          <ul className="flex gap-6">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
         {children}
       </body>
     </html>
