c1c3655f02d36965386bc3899110dda764d26827
skmajumder <skmajumder.cse@gmail.com>
2024-05-13 12:45:45 +0600

    Add loading js

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function AboutLayout({ children }) {
   return (
     <div>
-      <nav>Mission | Vission</nav>
+      <nav>
+        <ul className="flex gap-6">
+          <li>
+            <Link href="/about/mission">Mission</Link>
+          </li>
+          <li>
+            <Link href="/about/vission">Vission</Link>
+          </li>
+        </ul>
+      </nav>
       {children}
     </div>
   );
