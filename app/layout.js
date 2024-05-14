43356e492e16b15cd1fed582b33ae1bcda87a45d
skmajumder <skmajumder.cse@gmail.com>
2024-05-14 18:05:51 +0600

    Font optimization

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} p-6`}>
+      <body className={`${poppins.className} p-6`}>
         <nav>
           <ul className="flex gap-6">
             <li>
