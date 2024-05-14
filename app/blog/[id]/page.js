6114658937b0d2541cfc38048ec1823c45192685
skmajumder <skmajumder.cse@gmail.com>
2024-05-14 10:43:40 +0600

    Single blog post

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,8 +1,18 @@
+import { notFound } from "next/navigation";
+import { blogPosts } from "../blogData";
+
 export default function Page({ params }) {
   const { id } = params;
+  const blogPost = blogPosts.find((blog) => blog.id === parseFloat(id));
+
+  if (!blogPost) {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Blog post ID: {id}</h1>
+      <h1>{blogPost?.title}</h1>
+      <p>{blogPost?.description}</p>
     </div>
   );
 }
---
+++ app/blog/blogData.js
@@ -0,0 +1,77 @@
+export const blogPosts = [
+  {
+    id: 10,
+    title: "Introduction to JavaScript",
+    description: "Learn the basics of JavaScript programming language.",
+    author: "John Doe",
+    publishDate: "2024-05-12",
+  },
+  {
+    id: 11,
+    title: "Mastering React.js",
+    description:
+      "Advanced techniques and best practices in React.js development.",
+    author: "Jane Smith",
+    publishDate: "2024-05-10",
+  },
+  {
+    id: 12,
+    title: "Deep Dive into Node.js",
+    description: "Explore the core concepts of Node.js and its ecosystem.",
+    author: "Alex Johnson",
+    publishDate: "2024-05-08",
+  },
+  {
+    id: 13,
+    title: "CSS Tricks for Beautiful Designs",
+    description:
+      "Tips and tricks to enhance your CSS skills and create stunning designs.",
+    author: "Emily Brown",
+    publishDate: "2024-05-06",
+  },
+  {
+    id: 14,
+    title: "Getting Started with Python",
+    description: "A beginner's guide to Python programming language.",
+    author: "Michael Wilson",
+    publishDate: "2024-05-04",
+  },
+  {
+    id: 15,
+    title: "The Power of Data Science",
+    description: "Unlock the potential of data science and its applications.",
+    author: "Sophia Garcia",
+    publishDate: "2024-05-02",
+  },
+  {
+    id: 16,
+    title: "Exploring UI/UX Design Principles",
+    description:
+      "Understanding the fundamentals of UI/UX design for better user experiences.",
+    author: "William Taylor",
+    publishDate: "2024-04-30",
+  },
+  {
+    id: 17,
+    title: "JavaScript Frameworks Comparison",
+    description:
+      "Comparing popular JavaScript frameworks: Angular, React, and Vue.",
+    author: "Olivia Martinez",
+    publishDate: "2024-04-28",
+  },
+  {
+    id: 18,
+    title: "Web Security Best Practices",
+    description:
+      "Essential security practices to protect your web applications from threats.",
+    author: "Ethan Adams",
+    publishDate: "2024-04-26",
+  },
+  {
+    id: 19,
+    title: "Introduction to Machine Learning",
+    description: "An overview of machine learning concepts and algorithms.",
+    author: "Ava Anderson",
+    publishDate: "2024-04-24",
+  },
+];
